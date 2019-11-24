2636b381cc376a6dd89b64eaa0b94ef69aab085a
Cleveri <52777114+Cleveri@users.noreply.github.com>
2019-11-24 17:40:49 +0200

    Invetory and Player
    Changed inventory frame and fixed player jumping

--- src/actors/scripts/Player.gd
+++ src/actors/scripts/Player.gd
@@ -14,14 +14,15 @@ func pick_up_item(item):
 	else:
 		return false
 
-func _unhandled_input(event):
-	if event.is_action_pressed("jump"):
-		velocity.y = -jumping_height
 
 func _physics_process(delta):
 	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	if direction < 0: $character.flip_h = true
 	elif direction > 0: $character.flip_h = false
+	
+	if Input.is_action_pressed("jump"):
+		if is_on_floor() :
+			velocity.y = -jumping_height
 	
 	velocity.x = direction * movement_speed
 	
