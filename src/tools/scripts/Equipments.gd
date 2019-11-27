06b99089ac8e57360c3b469fcb517938d5f62a54
Ibrahim Elaradi <44804830+Darkystel@users.noreply.github.com>
2019-11-27 14:24:16 +0200

    Update Equipments.gd

--- src/tools/scripts/Equipments.gd
+++ src/tools/scripts/Equipments.gd
@@ -9,9 +9,17 @@ func equip(item: Item):
 		var item_scene = item.item_scene.instance()
 		item_scene.item_resource = currently_equipped
 		add_child(item_scene)
+	elif item != null:
+		unequip()
+		currently_equipped = item
+		currently_equipped.connect("unequip", self, "unequip")
+		var item_scene = item.item_scene.instance()
+		item_scene.item_resource = currently_equipped
+		add_child(item_scene)
 	else:
 		unequip()
 
 func unequip():
 	get_child(get_child_count()-1).queue_free()
+	currently_equipped.disconnect("unequip", self, "unequip")
 	currently_equipped = null
