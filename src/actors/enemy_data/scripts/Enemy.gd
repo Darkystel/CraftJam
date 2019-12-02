6739bc50312a8cba70d1efc26c93c5e5e2f852d0
Ibrahim Elaradi <44804830+Darkystel@users.noreply.github.com>
2019-12-03 01:35:23 +0200

    Added finish game

--- src/actors/enemy_data/scripts/Enemy.gd
+++ src/actors/enemy_data/scripts/Enemy.gd
@@ -19,6 +19,8 @@ onready var animator = $animator
 onready var spawn_position = self.position
 ####################################
 
+signal enemy_died
+
 func _physics_process(delta):
 	for body in $damage_area.get_overlapping_bodies():
 		if body.is_in_group("player"):
--- src/actors/enemy_data/scripts/states/die.gd
+++ src/actors/enemy_data/scripts/states/die.gd
@@ -7,4 +7,5 @@ func _enter():
 	_root.play_animation("death")
 
 func _on_animator_animation_finished(animation):
+	_root.emit_signal("enemy_died")
 	_root.queue_free()
