a74f5441b7f0ef8acf82302f3691dc38306fac07
Cleveri <52777114+Cleveri@users.noreply.github.com>
2020-01-25 00:48:15 +0200

    fixed everything

--- src/UI/scripts/UI_controller.gd
+++ src/UI/scripts/UI_controller.gd
@@ -4,7 +4,7 @@ onready var overlay = get_parent()
 var inventory_resource = preload("res://src/UI/inventory_jrj.tscn")
 var inventory = inventory_resource.instance()
 func _unhandled_input(event):
-	if event.is_action_pressed("inventory"):
+	if event.is_action_pressed("inventory") and not popping_overlay:
 		#inventory.show()
 		#overlay.env.add_child(inventory)
 		push_overlay(_INVENTORY)
